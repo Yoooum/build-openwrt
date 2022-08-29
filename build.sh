4165f9e25affb8d04634d617e2be5096101d1997
未確認の庭師 <53594138+Yoooum@users.noreply.github.com>
2022-08-29 22:50:06 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,5 +9,5 @@ mv ${OP_BUILD_PATH}/.config ${OP_BUILD_PATH}/immortalwrt/.config
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 make defconfig
 make download -j$(nproc) V=s
-# make -j$(nproc) || make -j1 || make -j1 V=s
+make -j$(nproc) || make -j1 || make -j1 V=s
 echo "FILE_DATE=$(date +%Y%m%d%H%M)" >>$GITHUB_ENV
