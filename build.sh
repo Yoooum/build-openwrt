e7242735e645a90ef7b7484296fe3cc3763458e4
未確認の庭師 <53594138+Yoooum@users.noreply.github.com>
2022-08-29 12:58:43 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ git clone -b "openwrt-21.02" --single-branch https://github.com/immortalwrt/immo
 cd ${OP_BUILD_PATH}/immortalwrt
 ./scripts/feeds update -a && ./scripts/feeds install -a
 rm -rf ./tmp && rm -rf .config
-cp ${OP_BUILD_PATH}/newifi_d2.config ${OP_BUILD_PATH}/immortalwrt/.config
+mv ${OP_BUILD_PATH}/newifi_d2.config ${OP_BUILD_PATH}/immortalwrt/.config
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 make defconfig
-make download -j$(nproc) V=s
+make download -j8
 make -j$(nproc) || make -j1 || make -j1 V=s
